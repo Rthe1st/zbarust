f2dd88da204c14012f196e751c6283f0d7ce8b2d
Mehow <Kaiser.mehow@gmail.com>
2020-02-28 20:41:05 +0000

    Exposing private refcnt functions

--- zbar/zbar/refcnt.h
+++ zbar/zbar/refcnt.h
@@ -23,40 +23,21 @@
 #ifndef _REFCNT_H_
 #define _REFCNT_H_
 
-#include <config.h>
-#include <assert.h>
-
 #if defined(_WIN32)
 # include <windows.h>
 
 typedef LONG refcnt_t;
 
-static inline int _zbar_refcnt (refcnt_t *cnt,
-                                int delta)
-{
-    int rc = -1;
-    if(delta > 0)
-        while(delta--)
-            rc = InterlockedIncrement(cnt);
-    else if(delta < 0)
-        while(delta++)
-            rc = InterlockedDecrement(cnt);
-    assert(rc >= 0);
-    return(rc);
-}
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta);
 
 #elif defined(TARGET_OS_MAC)
 # include <libkern/OSAtomic.h>
 
 typedef int32_t refcnt_t;
 
-static inline int _zbar_refcnt (refcnt_t *cnt,
-                                int delta)
-{
-    int rc = OSAtomicAdd32Barrier(delta, cnt);
-    assert(rc >= 0);
-    return(rc);
-}
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta);
 
 #elif defined(HAVE_LIBPTHREAD)
 # include <pthread.h>
@@ -65,28 +46,16 @@ typedef int refcnt_t;
 
 extern pthread_mutex_t _zbar_reflock;
 
-static inline int _zbar_refcnt (refcnt_t *cnt,
-                                int delta)
-{
-    pthread_mutex_lock(&_zbar_reflock);
-    int rc = (*cnt += delta);
-    pthread_mutex_unlock(&_zbar_reflock);
-    assert(rc >= 0);
-    return(rc);
-}
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta);
 
 
 #else
 
 typedef int refcnt_t;
 
-static inline int _zbar_refcnt (refcnt_t *cnt,
-                                int delta)
-{
-    int rc = (*cnt += delta);
-    assert(rc >= 0);
-    return(rc);
-}
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta);
 
 #endif
 
