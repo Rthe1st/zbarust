03dee4d75652376ff67f682d0217e39808a08567
Mehow <Kaiser.mehow@gmail.com>
2020-02-28 20:41:05 +0000

    Exposing private refcnt functions

--- zbar/zbar/refcnt.c
+++ zbar/zbar/refcnt.c
@@ -23,6 +23,65 @@
 
 #include "refcnt.h"
 
+#include <config.h>
+#include <assert.h>
+
+#if defined(_WIN32)
+# include <windows.h>
+
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta)
+{
+    int rc = -1;
+    if(delta > 0)
+        while(delta--)
+            rc = InterlockedIncrement(cnt);
+    else if(delta < 0)
+        while(delta++)
+            rc = InterlockedDecrement(cnt);
+    assert(rc >= 0);
+    return(rc);
+}
+
+#elif defined(TARGET_OS_MAC)
+# include <libkern/OSAtomic.h>
+
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta)
+{
+    int rc = OSAtomicAdd32Barrier(delta, cnt);
+    assert(rc >= 0);
+    return(rc);
+}
+
+#elif defined(HAVE_LIBPTHREAD)
+# include <pthread.h>
+
+extern pthread_mutex_t _zbar_reflock;
+
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta)
+{
+    pthread_mutex_lock(&_zbar_reflock);
+    int rc = (*cnt += delta);
+    pthread_mutex_unlock(&_zbar_reflock);
+    assert(rc >= 0);
+    return(rc);
+}
+
+
+#else
+
+int _zbar_refcnt (refcnt_t *cnt,
+                                int delta)
+{
+    int rc = (*cnt += delta);
+    assert(rc >= 0);
+    return(rc);
+}
+
+#endif
+
 #if !defined(_WIN32) && !defined(TARGET_OS_MAC) && defined(HAVE_LIBPTHREAD)
 
 pthread_once_t initialized = PTHREAD_ONCE_INIT;
